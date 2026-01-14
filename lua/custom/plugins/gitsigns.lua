9400011dffe0e47a045d8b662ac11083c2af1ebd
Braden <braden.fullmer@gmail.com>
2026-01-13 21:26:22 -0700

    commiting my updates for my kickstart neovim

---
+++ lua/custom/plugins/gitsigns.lua
@@ -0,0 +1,17 @@
+return
+{
+    -- See `:help gitsigns` to understand what the configuration keys do
+    -- Adds git related signs to the gutter, as well as utilities for managing changes
+    -- Here is a more advanced example where we pass configuration
+    -- options to `gitsigns.nvim`.
+        'lewis6991/gitsigns.nvim',
+        opts = {
+            signs = {
+                add = { text = '+' },
+                change = { text = '~' },
+                delete = { text = '_' },
+                topdelete = { text = '‾' },
+                changedelete = { text = '~' },
+            },
+        },
+}
